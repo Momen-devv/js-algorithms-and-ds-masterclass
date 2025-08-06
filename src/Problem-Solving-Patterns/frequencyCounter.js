c7804400ced6e58104696eb2022a19da3e3de7b8
Momen <momen.devv@gmail.com>
2025-08-06 12:40:52 +0300

    Frequency counter

--- src/Problem-Solving-Patterns/frequencyCounter.js
+++ src/Problem-Solving-Patterns/frequencyCounter.js
@@ -3,7 +3,7 @@
 //[1 ,2 ,1], [4 , 4, 1] false
 
 // Try without frequency counter
-function same(arr1, arr2) {
+function same1(arr1, arr2) {
   if (arr1.length != arr2.length) return false
 
   // loop on the arry 1
@@ -23,4 +23,32 @@ function same(arr1, arr2) {
   return arr2.length === 0
 }
 
-console.log(same([1, 2, 1], [4, 4, 1]))
+console.log(same1([1, 2, 1], [4, 4, 1]))
+
+// Frequency counter
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+  let frequencyCounter1 = {}
+  let frequencyCounter2 = {}
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+  }
+  console.log(frequencyCounter1)
+  console.log(frequencyCounter2)
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false
+    }
+  }
+  return true
+}
+
+same([1, 2, 3, 2, 5], [9, 1, 4, 4, 11])
