9d46a392f16d283be47a55cb5a18d513fb279427
Momen <momen.devv@gmail.com>
2025-08-07 07:41:31 +0300

    Count Unique Values Challenge

--- src/Problem-Solving-Patterns/multiplePointers.js
+++ src/Problem-Solving-Patterns/multiplePointers.js
@@ -28,5 +28,29 @@ function sumZero1(array) {
   return undefined
 }
 
-console.log(sumZero1([-3, -2, -1, 0, 1, 2, 3]))
-console.log(sumZero1([-4, -3, -2, -1, 0, 7, 6, 5, 10]))
+// console.log(sumZero1([-3, -2, -1, 0, 1, 2, 3]))
+// console.log(sumZero1([-4, -3, -2, -1, 0, 7, 6, 5, 10]))
+
+// Multiple Pointers: Count Unique Values Challenge
+// First come in my mind
+function countUniqueValues(array) {
+  // make object
+  let object = {}
+  let result = []
+  // loop in array and push it to array if exict add one if not = 1
+  for (let num in array) {
+    object[array[num]] ? (object[array[num]] += 1) : (object[array[num]] = 1)
+  }
+  console.log(object)
+  // loop in object and check values if = 1 then add it to the arrry
+
+  Object.entries(object).forEach(([key, value]) => {
+    if (value === 1) {
+      result.push(key)
+    }
+  })
+  // return the array with finle unique numbers
+  return result
+}
+
+console.log(countUniqueValues([1, 1, 1, 1, 1, 1, 2, 7, 4, 4]))
