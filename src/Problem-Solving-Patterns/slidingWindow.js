8e90a904ee19b3b24fab1456ece6f04a62d4f01a
Momen <momen.devv@gmail.com>
2025-08-08 10:17:07 +0300

    Sliding Window Pattern with colt

--- src/Problem-Solving-Patterns/slidingWindow.js
+++ src/Problem-Solving-Patterns/slidingWindow.js
@@ -42,4 +42,22 @@ function maxSubarraySum1(arr, num) {
   return max
 }
 
-console.log(maxSubarraySum1([2, 6, 9, 2, 1, 8, 5, 6, 3], 3)) // 19
+// console.log(maxSubarraySum1([2, 6, 9, 2, 1, 8, 5, 6, 3], 3)) // 19
+
+// Sliding Window Pattern with colt
+function maxSubarraySum3(arr, num) {
+  let maxSum = 0
+  let tempSum = 0
+  if (arr.length < num) return null
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i]
+  }
+  tempSum = maxSum
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i]
+    maxSum = Math.max(maxSum, tempSum)
+  }
+  return maxSum
+}
+
+console.log(maxSubarraySum3([2, 6, 9, 2, 1, 8, 5, 6, 3], 3))
